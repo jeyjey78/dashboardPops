8a2c84b8ad8b032edbb33d05c9ea4a2e3dea2876
Jeremy gros <jeremy@aloa.us>
2017-03-02 14:58:54 +0100

    Fix research Batch + export batch

--- exports/exports.js
+++ exports/exports.js
@@ -11,6 +11,7 @@ angular.module('myApp.exports', ['ngRoute'])
 
 .controller('ExportsCtrl', ["$scope", '$routeParams', '$location', '$http', '$cookieStore', 'user', 'alertView', 'server', function($scope, $routeParams, $location, $http, $cookieStore, user, alertView, server) {
 	$scope.batches = []
+  $scope.statusBatch = []
 	$scope.statusOptions = [{
 		status: "pending"
 	},{
@@ -28,6 +29,7 @@ angular.module('myApp.exports', ['ngRoute'])
 	}]
 
   $scope.newExportBtn = false
+  $scope.exportDisable = true
   $scope.rights = user.rights
 
 	$scope.getWaitingOrders = function(){
@@ -64,6 +66,12 @@ angular.module('myApp.exports', ['ngRoute'])
 			if (response["data"]) {
 				$scope.batches = response["data"] != null ? response["data"] : ""
         $scope.statusSelected = $scope.statusOptions[0]
+        if ($scope.statusSelected["status"] == "downloadable" || $scope.statusSelected["status"] == "downloaded") {
+          $scope.exportDisable = false
+        }
+        else {
+          $scope.exportDisable = true
+        }
 			}
 			else {
 				$(".alertDiv").append("<div class='alert alert-danger'>"+response["errorMessage"]+"</div>")
@@ -97,12 +105,40 @@ angular.module('myApp.exports', ['ngRoute'])
 
   $scope.cancelBatch = function (batchId) {
     console.log(batchId)
+    if (confirm("Do you really want to cancel this batch?")) {
+      $scope.loading = true
+      $http({method: "POST", url: server.urlDev+'batches/'+batchId+"/cancel",headers: {'sessionToken': user.sessionToken}}).success(function successCallback(response) {
+  			console.log(response)
+  			$scope.loading = false
+  			if (response["data"]) {
+  				$scope.searchBatches()
+  			}
+  			else {
+  				$(".alertDiv").append("<div class='alert alert-danger'>"+response["errorMessage"]+"</div>")
+  				alertView.error()
+  			}
+  		}, function errorCallback(response) {
+  			alert("ERROR")
+  		});
+    }
+  }
+
+  $scope.searchBatches = function () {
     $scope.loading = true
-    $http({method: "POST", url: server.urlDev+'batches/'+batchId+"/cancel",headers: {'sessionToken': user.sessionToken}}).success(function successCallback(response) {
+    $http({method: "GET", url: server.urlDev+'batches/?batchStatus='+$scope.statusSelected["status"],headers: {'sessionToken': user.sessionToken}}).success(function successCallback(response) {
 			console.log(response)
 			$scope.loading = false
 			if (response["data"]) {
-				$scope.searchBatches()
+				$scope.batches = response["data"] != null ? response["data"] : ""
+        // console.log($scope.batches["batchStatus"])
+        // console.log($scope.getStatusIndex($scope.batches["batchStatus"]))
+        // $scope.batchStatus = $scope.statusOptions[$scope.getStatusIndex($scope.batches["batchStatus"])]
+        if ($scope.statusSelected["status"] == "downloadable" || $scope.statusSelected["status"] == "downloaded") {
+          $scope.exportDisable = false
+        }
+        else {
+          $scope.exportDisable = true
+        }
 			}
 			else {
 				$(".alertDiv").append("<div class='alert alert-danger'>"+response["errorMessage"]+"</div>")
@@ -113,21 +149,49 @@ angular.module('myApp.exports', ['ngRoute'])
 		});
   }
 
-  $scope.searchBatches = function () {
+  $scope.exportBatches = function(batchId) {
     $scope.loading = true
-    $http({method: "GET", url: server.urlDev+'batches/?batchStatus='+$scope.statusSelected["status"],headers: {'sessionToken': user.sessionToken}}).success(function successCallback(response) {
-			console.log(response)
-			$scope.loading = false
-			if (response["data"]) {
-				$scope.batches = response["data"] != null ? response["data"] : ""
-			}
-			else {
-				$(".alertDiv").append("<div class='alert alert-danger'>"+response["errorMessage"]+"</div>")
-				alertView.error()
-			}
-		}, function errorCallback(response) {
-			alert("ERROR")
-		});
+    $http({method: "GET", url: server.urlDev+'batches/'+batchId+"/zipUrl",headers: {'sessionToken': user.sessionToken}}).success(function successCallback(response) {
+      console.log(response)
+      $scope.loading = false
+      if (response["data"]) {
+        $scope.batches = response["data"] != null ? response["data"] : ""
+      }
+      else {
+        $(".alertDiv").append("<div class='alert alert-danger'>"+response["errorMessage"]+"</div>")
+        alertView.error()
+      }
+    }, function errorCallback(response) {
+      alert("ERROR")
+    });
   }
+
+  $scope.updateStatusBatches = function(index, batchId) {
+    $scope.loading = true
+    var data = {"batchStatus":$scope.statusBatch[index]["status"]}
+    $http({method: "POST", url: server.urlDev+'batches/'+batchId+"/status",data: data,headers: {'sessionToken': user.sessionToken}}).success(function successCallback(response) {
+      console.log(response)
+      $scope.loading = false
+      if (response["data"]) {
+        //$scope.batches = response["data"] != null ? response["data"] : ""
+        $scope.searchBatches()
+      }
+      else {
+        $(".alertDiv").append("<div class='alert alert-danger'>"+response["errorMessage"]+"</div>")
+        alertView.error()
+      }
+    }, function errorCallback(response) {
+      alert("ERROR")
+    });
+  }
+
+  /*** Utilities ***/
+  $scope.getStatusIndex = function(status) {
+		for(var i=0; i<$scope.statusOptions.length; i++) {
+        	if($scope.statusOptions[i]["status"] == status) {
+             	return i
+             }
+        }
+	}
 
 }]);
